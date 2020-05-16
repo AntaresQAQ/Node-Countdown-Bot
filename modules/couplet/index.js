c957f882537da1de3505c302b623d6357dccd94a
Antares <antares.oier@gmail.com>
2020-05-17 01:30:23 +0800

    module couplet

---
+++ modules/couplet/index.js
@@ -0,0 +1,26 @@
+const requestPromise = require("request-promise");
+const urlencode = require("urlencode");
+
+bot.command("couplet <text>", "对对联")
+    .alias("对联")
+    .usage("couplet [上联]")
+    .action(async ({meta}, text) => {
+        try {
+            // Supported by https://ai.binwang.me/couplet/
+            let result = await requestPromise.get({
+                uri: `https://ai-backend.binwang.me/chat/couplet/${urlencode(text)}`,
+                json: true,
+                timeout: 30000
+            });
+            await meta.$send("上联：" + text + "\n下联：" + result.output);
+        } catch (e) {
+            CountdownBot.log(e, meta.$send);
+        }
+    });
+
+
+module.exports = {
+    author: "Antares",
+    version: 1.0,
+    description: "对联机"
+};
