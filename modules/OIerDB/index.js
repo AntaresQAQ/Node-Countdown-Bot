d7a7cf8d08a3447731262d6dd5a5a7de7a92eca4
Antares <antares.oier@gmail.com>
2020-06-05 15:33:17 +0800

    using vm

--- modules/OIerDB/index.js
+++ modules/OIerDB/index.js
@@ -1,4 +1,5 @@
 const requestPromise = require("request-promise");
+const {VM} = require("vm2");
 
 
 bot.command("oier <name...>", "OIerDB(http://bytew.net/OIer)查询")
@@ -14,6 +15,10 @@ bot.command("oier <name...>", "OIerDB(http://bytew.net/OIer)查询")
                 json: true,
                 contentType: ""
             });
+            let vm = new VM({
+                sandbox: {},
+                timeout: 1000
+            });
             let items = result.result.sort(() => (Math.random() - 0.5));
             if (!items.length) throw new ErrorMsg("找不到相关内容", meta);
             let buffers = [Buffer.from("查询到以下内容:\n")];
@@ -21,7 +26,8 @@ bot.command("oier <name...>", "OIerDB(http://bytew.net/OIer)查询")
                 let item = items[i];
                 buffers.push(Buffer.from(
                     `\n姓名: ${item.name}\n生理性别: ${{"-1": "女", "1": "男"}[item.sex] || "未知"}\n`));
-                for (let award of eval(item.awards)) {
+                let awards = vm.run(item.awards);
+                for (let award of awards) {
                     buffers.push(Buffer.from(
                         `在<${award.province}>${award.school}<${award.grade}>时参加` +
                         `<${award.identity}>以${award.score}分(全国排名${award.rank})的成绩获得<${award.award_type}>\n`));
