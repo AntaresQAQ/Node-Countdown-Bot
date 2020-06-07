76b5f61ee4047eaf8af682660ab970950d1c6b17
Antares <antares.oier@gmail.com>
2020-06-07 22:27:11 +0800

    add headers

--- modules/DogsDairy/index.js
+++ modules/DogsDairy/index.js
@@ -9,7 +9,15 @@ bot.command("dog", "舔狗日记(https://we.dog)")
             let res = await requestPromise.get({
                 uri: "https://we.dog/assets/js/index.js",
                 json: false,
-                timeout: 100000
+                timeout: 100000,
+                header: {
+                    "Accept": "application/json, text/plain, */*",
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) " +
+                        "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                        "Chrome/81.0.4044.122 Safari/537.36",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept-Language": "zh-CN,zh;q=0.9"
+                }
             });
             let vm = new VM({
                 sandbox: {},
