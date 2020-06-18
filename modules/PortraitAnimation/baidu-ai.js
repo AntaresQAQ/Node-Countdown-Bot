822a0b34a5a229eee2fb8b37070d24227f319eea
Antares <antares.oier@gmail.com>
2020-06-18 15:28:34 +0800

    module PortraitAnimation

---
+++ modules/PortraitAnimation/baidu-ai.js
@@ -0,0 +1,51 @@
+const requestPromise = require("request-promise");
+const schedule = require("node-schedule");
+const querystring = require("querystring");
+
+class BaiduAI {
+    constructor(ClientId, ClientSecret) {
+        let getToken = async () => {
+            try {
+                let res = await requestPromise.post({
+                    uri: "https://aip.baidubce.com/oauth/2.0/token",
+                    qs: {
+                        grant_type: "client_credentials",
+                        client_id: ClientId,
+                        client_secret: ClientSecret
+                    },
+                    json: true
+                });
+                if (res.error === "invalid_client") throw new Error(res.error_description);
+                this.AccessToken = res.access_token;
+                console.log("PortraitAnimation: Get Baidu Token Succeed!");
+                let nextDate = new Date(Date.now() + parseInt(res.expires_in) * 1000 - 60000);
+                schedule.scheduleJob(nextDate, getToken);
+            } catch (e) {
+                console.error("PortraitAnimation: Get Baidu Token Failed, Check Your ClientId and ClientSecret!");
+                console.error(e);
+                process.exit(1);
+            }
+        }
+        getToken().catch((err) => {
+            console.error(err);
+            process.exit(1);
+        });
+    }
+
+    async portraitAnimation(image, mask_id) {
+        let postBody = {image: image};
+        if (mask_id) {
+            postBody.type = "anime_mask";
+            postBody.mask_id = mask_id;
+        }
+        let res = await requestPromise.post({
+            uri: "https://aip.baidubce.com/rest/2.0/image-process/v1/selfie_anime",
+            header: {"Content-Type": "application/x-www-form-urlencoded"},
+            qs: {access_token: this.AccessToken},
+            body: querystring.stringify(postBody)
+        });
+        return JSON.parse(res);
+    }
+}
+
+module.exports = BaiduAI;
---
+++ modules/PortraitAnimation/index.js
@@ -0,0 +1,60 @@
+const configDefault = {
+    baidu_client_id: "xxxxxxxx",
+    baidu_client_secret: "xxxxxxxx"
+};
+const config = CountdownBot.loadConfig(__dirname, configDefault);
+
+const requestPromise = require("request-promise");
+const BaiduAI = require("./baidu-ai.js");
+const client = new BaiduAI(config.baidu_client_id, config.baidu_client_secret);
+
+bot.command("anime <...image>", "人像动漫化")
+    .alias("动画化")
+    .option("-m,--mask", "口罩ID (1-8)")
+    .action(async ({meta, options}, image) => {
+        try {
+            if (options.mask) {
+                let maskId = parseInt(options.mask);
+                if (isNaN(maskId) || maskId < 1 || maskId > 8) throw new ErrorMsg("非法的口罩ID", meta);
+            }
+            const imageRE = /^\[CQ:image,file=([^,]+),url=([^\]]+)]$/g;
+            let search = imageRE.exec(image);
+            if (!search) {
+                await meta.$send(`[CQ:at,qq=${meta.userId}] 请发送一张图片`);
+                bot.onceMiddleware(async (meta1, next) => {
+                    try {
+                        search = imageRE.exec(meta1.message);
+                        if (!search) next();
+                        let imageUrl = search[2];
+                        let imageBase64 = await requestPromise.get({
+                            uri: imageUrl,
+                            encoding: "base64"
+                        });
+                        let result = await client.portraitAnimation(imageBase64, options.mask);
+                        console.log(result);
+                        await meta1.$send(`[CQ:image,file=base64://${result.image}]`);
+                    } catch (e) {
+                        CountdownBot.log(e);
+                    }
+                }, meta);
+                return;
+            }
+            let imageUrl = search[2];
+            let imageBase64 = await requestPromise.get({
+                uri: imageUrl,
+                encoding: "base64"
+            });
+            let result = await client.portraitAnimation(imageBase64, options.mask);
+            console.log(result);
+            await meta.$send(`[CQ:image,file=base64://${result.image}]`);
+        } catch (e) {
+            CountdownBot.log(e);
+        }
+    });
+
+module.exports = {
+    author: "Antares",
+    version: "1.0",
+    description: "人像动画化"
+}
+
