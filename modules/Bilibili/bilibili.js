90b14ee5348f332914f6bfda0b858d91a02d70be
Antares <antares.oier@gmail.com>
2020-06-24 20:48:40 +0800

    Bilibili QqApp to AvNumber

---
+++ modules/Bilibili/bilibili.js
@@ -0,0 +1,37 @@
+const request = require("request");
+const Promise = require("bluebird");
+const Url = require("url");
+const path = require("path");
+
+const tab = [...'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'];
+const sss = [11, 10, 3, 8, 4, 6];
+const xor = 177451812;
+const add = 8728348608;
+
+module.exports = {
+    av2bv(av) {
+        let num = parseInt(av);
+        if (isNaN(num) || num <= 0) return null;
+        num = (num ^ xor) + add;
+        let result = [...'BV1  4 1 7  '];
+        for (let i = 0; i < 6; i++) {
+            result[sss[i]] = tab[Math.floor(num / 58 ** i) % 58];
+        }
+        return result.join('');
+    },
+    bv2av(bv) {
+        let result = 0;
+        for (let i = 0; i < 6; i++) {
+            result += tab.indexOf(bv[sss[i]]) * 58 ** i;
+        }
+        return `av${result - add ^ xor}`;
+    },
+    url2bv(url) {
+        return new Promise((resolve, reject) => {
+            request(url, {followAllRedirects: true}, (err, res) => {
+                if (err) reject(err);
+                resolve(path.parse(Url.parse(res.request.href).pathname).name);
+            });
+        });
+    }
+}
---
+++ modules/Bilibili/index.js
@@ -0,0 +1,51 @@
+const configDefault = {
+    "using_av": false
+};
+
+const config = CountdownBot.loadConfig(__dirname, configDefault);
+
+const Bilibili = require("./bilibili.js");
+const {CQCode} = require("koishi");
+
+bot.command("avbv <id>", "AV号BV号互转")
+    .action(async ({meta}, id) => {
+        try {
+            const avRE = /[Aa][Vv]([0-9]+)/g;
+            const bvRE = /[Bb][Vv][fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF]{10}/g;
+            let search = avRE.exec(id);
+            if (search) {
+                let bv = Bilibili.av2bv(search[1]);
+                if (!bv) throw new ErrorMsg("非法的AV号", meta);
+                await meta.$send(bv);
+                return;
+            }
+            search = bvRE.exec(id);
+            if (!search) throw new ErrorMsg("非法的AV号或BV号", meta);
+            await meta.$send(Bilibili.bv2av(search[0]));
+        } catch (e) {
+            CountdownBot.log(e);
+        }
+    });
+
+bot.groups.receiver.on("message", async (meta) => {
+    try {
+        let search = /\[CQ:rich,content=([^,]+),title=([^,]+)]/g.exec(meta.message);
+        if (!search) return;
+        let content = JSON.parse(CQCode.unescape(search[1]));
+        if (content.detail_1.appid !== "1109937557") return;
+        let bv = await Bilibili.url2bv(content.detail_1.qqdocurl)
+        if (config.using_av) {
+            await meta.$send(Bilibili.bv2av(bv));
+        } else {
+            await meta.$send(bv);
+        }
+    } catch (e) {
+        CountdownBot.log(e);
+    }
+});
+
+module.exports = {
+    author: "Antares",
+    version: "1.0",
+    description: "Bilibili插件"
+}
