02734bce8502759038bf1b848a05bac085b80cea
Antares <antares.oier@gmail.com>
2020-09-02 16:50:52 +0800

    disable https reject unauthorized

--- modules/Couplet/index.js
+++ modules/Couplet/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const urlencode = require("urlencode");
+const https = require("https");
 
 bot.command("couplet <text>", "对对联")
   .alias("对联")
@@ -8,7 +9,8 @@ bot.command("couplet <text>", "对对联")
     try {
       // Supported by https://ai.binwang.me/couplet/
       let result = await axios.get(`https://ai-backend.binwang.me/chat/couplet/${urlencode(text)}`, {
-        timeout: 30000
+        timeout: 30000,
+        httpsAgent: new https.Agent({rejectUnauthorized: false})
       });
       await meta.$send("上联：" + text + "\n下联：" + result.data.output);
     } catch (e) {
