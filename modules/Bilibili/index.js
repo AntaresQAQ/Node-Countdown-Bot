1dd6cea4b338cfd184b7efa999352ccf47e1cf04
Antares <antares.oier@gmail.com>
2020-09-22 01:01:15 +0800

    fix bug

--- modules/Bilibili/index.js
+++ modules/Bilibili/index.js
@@ -31,7 +31,10 @@ bot.groups.receiver.on("message", async (meta) => {
   try {
     let search = /^(&#91;&#91;QQ小程序&#93;哔哩哔哩&#93;请使用最新版本手机QQ查看)?\[CQ:json,data=([^\]]+)]$/g.exec(meta.message);
     if (!search) return;
-    let content = JSON.parse(CQCode.unescape(search[2]))["meta"]["detail_1"];
+    let content = JSON.parse(CQCode.unescape(search[2]))["meta"];
+    if (!content) return;
+    content = content["detail_1"];
+    if (!content) return;
     if (content["appid"] !== "1109937557") return;
     let bv = await Bilibili.url2bv(content["qqdocurl"]);
     if (config.using_av) {
