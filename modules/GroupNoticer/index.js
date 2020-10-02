fecf6d75aa2f57e318bbe2fd801de546743da4f2
Antares <antares.oier@gmail.com>
2020-10-02 22:14:16 +0800

    fix bug

--- modules/GroupNoticer/index.js
+++ modules/GroupNoticer/index.js
@@ -23,16 +23,13 @@ bot.groups.receiver.on("group-increase", async (meta) => {
 
 bot.groups.receiver.on("group-decrease", async (meta) => {
   try {
-    // let leftUser = await bot.sender.getStrangerInfo(meta.userId);
+    let leftUser = await bot.sender.getStrangerInfo(meta.userId);
     if (meta.subType === "leave") {
-      // await bot.sender.sendGroupMsgAsync(meta.groupId,
-      //   `用户 ${leftUser.nickname}(${leftUser.userId}) 已离开本群`);
-      await bot.sender.sendGroupMsgAsync(meta.groupId, `用户 ${meta.userId} 已离开本群`);
-    } else if (meta.subType === "kick") {
-      // await bot.sender.sendGroupMsgAsync(meta.groupId,
-      //   `用户 ${leftUser.nickname}(${leftUser.userId}) 被 [CQ:at,qq=${meta.operatorId}] 踢出本群`);
       await bot.sender.sendGroupMsgAsync(meta.groupId,
-        `用户 ${meta.userId} 被 [CQ:at,qq=${meta.operatorId}] 踢出本群`);
+        `用户 ${leftUser.nickname}(${leftUser.userId}) 已离开本群`);
+    } else if (meta.subType === "kick") {
+      await bot.sender.sendGroupMsgAsync(meta.groupId,
+        `用户 ${leftUser.nickname}(${leftUser.userId}) 被 [CQ:at,qq=${meta.operatorId}] 踢出本群`);
     } else {
       CountdownBot.log("Leave Group " + meta.groupId);
     }
